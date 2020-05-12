8288e8987f78e5c0ad06d9e2f1a29d16210d7c53
Matthias Mac <matthias-mac@refineddigital.co.nz>
2020-05-12 11:30:40 +0200

    wip

--- src/Module/Resources/views/front-end/fields/number.blade.php
+++ src/Module/Resources/views/front-end/fields/number.blade.php
@@ -4,6 +4,9 @@
   if (isset($defaultFields, $defaultFields[$field->field_name])) {
       $value = $defaultFields[$field->field_name];
   }
+  if (isset($defaultFields, $defaultFields[$field->custom_class])) {
+      $value = $defaultFields[$field->custom_class];
+  }
 @endphp
 {!!
     html()
