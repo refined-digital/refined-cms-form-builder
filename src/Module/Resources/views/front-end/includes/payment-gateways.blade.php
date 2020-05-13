ca440689f8702a5b0539cfb36fa36bb15bd46f6c
Matthias Mac <matthias-mac@refineddigital.co.nz>
2020-05-13 12:10:49 +0200

    wip

--- src/Module/Resources/views/front-end/includes/payment-gateways.blade.php
+++ src/Module/Resources/views/front-end/includes/payment-gateways.blade.php
@@ -6,7 +6,7 @@
   <div class="form__row form__row--payment-gateway">
     @foreach ($gateways as $type => $gateway)
       <div class="form__payment-gateway payment-gateway{{ $loop->first ? ' payment-gateway--active' : '' }} form__payment-gateway--{{ str_slug($type) }}">
-        <input type="radio" name="payment_gateway" id="form-payment-gateway-{{ str_slug($type) }}" value="{{str_slug($type)}}"{!! $loop->first ? ' checked' : '' !!} onchange="FormBuilder.paymentGatewayChanged(event)"/>
+        <input type="radio" name="payment_gateway" id="form-payment-gateway-{{ str_slug($type) }}" value="{{str_slug($type)}}"{!! $loop->first ? ' checked' : '' !!}/>
         <label class="form__label" for="form-payment-gateway-{{ str_slug($type) }}">{{ $type }}</label>
         <div class="payment-gateway__details">
           {!! view()->make($gateway->getView())->with(compact('form')) !!}
