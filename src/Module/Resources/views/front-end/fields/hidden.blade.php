5201193ea75c7fa9882a3901e66a7fe5adb93dc2
Matthias <matthias@refineddigital.co.nz>
2020-09-08 05:45:29 +0000

    feat: add default fields for hidden fields

--- src/Module/Resources/views/front-end/fields/hidden.blade.php
+++ src/Module/Resources/views/front-end/fields/hidden.blade.php
@@ -1,6 +1,17 @@
+@php
+  // todo: move this and don't do this in here
+  $value = $field->hidden_field_value ?: $field->data;
+  if (isset($form->defaultFields, $form->defaultFields[$field->field_name])) {
+      $value = $form->defaultFields[$field->field_name];
+  }
+  if (isset($form->defaultFields, $form->defaultFields[$field->custom_class])) {
+      $value = $form->defaultFields[$field->custom_class];
+  }
+@endphp
+
 {!!
     html()
         ->input('hidden', $field->field_name)
         ->attributes($field->attributes)
-        ->value($field->data)
+        ->value($value)
 !!}
