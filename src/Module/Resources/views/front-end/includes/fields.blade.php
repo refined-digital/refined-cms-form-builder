a607a0e380cdb0236b540204ee1199a33d12a66d
Matthias <matthias@refineddigital.co.nz>
2020-11-16 20:22:06 +0000

    fix: class on fields

--- src/Module/Resources/views/front-end/includes/fields.blade.php
+++ src/Module/Resources/views/front-end/includes/fields.blade.php
@@ -7,7 +7,7 @@
 @endif
 
 @if ($fields->fields->count())
-  <div class="form__fields--hidden">
+  <div class="form__fields--fields">
     @foreach ($fields->fields as $field)
       @if ($field->form_field_type_id == 11)
         {!! view('formBuilder::front-end.elements.row', ['field' => $field, 'errors' => $errors, 'defaultFields' => $form->defaultFields ?: []]) !!}
