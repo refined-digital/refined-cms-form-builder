897411b8055a8619689ffcd5916c03785781200c
Matthias Law <matthias@refineddigital.co.nz>
2022-01-14 11:34:48 +1300

    fix: change button output to be unescaped

--- src/Module/Resources/views/front-end/includes/submit.blade.php
+++ src/Module/Resources/views/front-end/includes/submit.blade.php
@@ -1,4 +1,4 @@
 <div class="form__row form__row--buttons">
   {!! Honeypot::generate('hname', 'htime') !!}
-  <button class="button">{{ $form->submitText }}</button>
+  <button class="button">{!! $form->submitText !!}</button>
 </div>
