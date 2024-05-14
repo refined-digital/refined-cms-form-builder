fda707615f6d6f5a425b6a53288dc9713b9bd6a7
Matthias Law <matthias@refineddigital.co.nz>
2024-05-15 08:28:16 +1200

    fix: validation and buttons for multiple forms

--- src/Module/Resources/views/front-end/includes/scripts.blade.php
+++ src/Module/Resources/views/front-end/includes/scripts.blade.php
@@ -1,9 +1,8 @@
 @section('scripts')
 <script src="{{ mix('/js/FormBuilder.js', '/vendor/refined/core') }}"></script>
 <script>
-  let form{{$form->id}} = document.querySelector('.form--{{ $form->id }}');
+  let form{{$form->id}} = document.querySelectorAll('.form--{{ $form->id }}');
   let validate{{$form->id}} = new window.FormValidate();
-  let buttons{{$form->id}} = document.querySelectorAll('.form--{{ $form->id }} .form__row--buttons .button');
 @if($form->recaptcha == 2)
   let formSubmitted{{ $form->id }} = false;
   function submitForm{{ $form->id }}() {
@@ -11,35 +10,34 @@
     form.submit();
   }
 @endif
-  form{{$form->id}}.addEventListener('submit', function(e) {
-    let errors{{$form->id}} = validate{{$form->id}}.validate(this);
-    if (buttons{{ $form->id }}) {
-      buttons{{ $form->id }}.forEach(item => {
-        item.classList.add('button--loading');
-      })
-    }
-
-    if (errors{{$form->id}}.length) {
-      e.preventDefault();
-      validate{{$form->id}}.alert();
-      if (buttons{{ $form->id }}) {
-        buttons{{ $form->id }}.forEach(item => {
-          item.classList.remove('button--loading');
-        })
+  form{{$form->id}}.forEach(form => {
+    form.addEventListener('submit', function (e) {
+      let submitButton = form.querySelector('.form__row--buttons .button');
+      let errors{{$form->id}} = validate{{$form->id}}.validate(this);
+      if (submitButton) {
+        submitButton.classList.add('button--loading');
       }
-    }
 
-    @yield('form-submit-injection')
-
-  @if($form->recaptcha == 2)
-    else {
-      if (!formSubmitted{{ $form->id }}) {
+      if (errors{{$form->id}}.length) {
         e.preventDefault();
-        grecaptcha.execute();
+        validate{{$form->id}}.alert();
+        if (submitButton) {
+          submitButton.classList.remove('button--loading');
+        }
       }
-    }
-  @endif
-});
+
+      @yield('form-submit-injection')
+
+              @if($form->recaptcha == 2)
+      else {
+        if (!formSubmitted{{ $form->id }}) {
+          e.preventDefault();
+          grecaptcha.execute();
+        }
+      }
+      @endif
+    });
+  });
 </script>
 
 @if($form->recaptcha)
