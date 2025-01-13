4e5cd4315cdaa484a1b86b68f69bece720cd0feb
Matthias Law <matthias@refineddigital.co.nz>
2025-01-13 17:36:42 +1300

    feat: adding page replacement field

--- src/Module/Fields/FormField_Hidden.php
+++ src/Module/Fields/FormField_Hidden.php
@@ -7,6 +7,11 @@ class FormField_Hidden extends FormField {
     public function render()
     {
         return <<<'blade'
+@php
+  if ($value === '[page]') {
+    $value = request()->url();
+  }
+@endphp
 {!!
     html()
         ->input('hidden', $field->field_name, $value)
